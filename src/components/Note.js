da6fcb199b8b2ec2e5511d5013132077a497ec88
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-03-23 15:21:50 -0400

    Update logic for styling, clean up code layout.

--- src/components/Note.js
+++ src/components/Note.js
@@ -2,13 +2,15 @@ import { Component } from 'react'
 
 class Note extends Component{
   render(){
-    const className = this.props.isActive ? 'note note-is-active' : 'note'
-    const styling = this.props.isActive ? {backgroundImage: `linear-gradient(rgba(${this.props.color[1]}, 1), rgba(${this.props.color[0]}, 1)`} : {backgroundImage: `linear-gradient(rgba(${this.props.color[1]}, 1), rgba(${this.props.color[1]}, 1)`}
+    const styling = 
+      this.props.isActive ? 
+        {backgroundImage: `linear-gradient(rgba(${this.props.color[1]}, 1), rgba(${this.props.color[0]}, 1)`} : 
+        {backgroundImage: `linear-gradient(rgba(${this.props.color[1]}, 1), rgba(${this.props.color[1]}, 1)`}
     return(
       <button 
       style={styling} 
       onClick={this.props.onClick}
-      className={className}>
+      className={this.props.className}>
       {/* {this.props.note} */}
       </button>
     )
