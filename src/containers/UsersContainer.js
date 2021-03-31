480d7366fe09dfa6388115f9f181419cafcb11f0
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-03-31 11:17:41 -0400

    Add Switch routing component.

--- src/containers/UsersContainer.js
+++ src/containers/UsersContainer.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import Login from '../components/Login'
+import SignUp from '../components/SignUp'
+import { createNewUser } from '../actions/currentUserActions'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 class UsersContainer extends Component {
+
+  handleFormSubmit = () => {
+    
+  }
+
 
   render(){
     return(
       <div className="container" id="users">
-        <Login />
+        <Switch>
+          <Route path='/signup' render={() => <SignUp createNewUser={this.props.createNewUser}/>} />
+          <Route path='/login' render={() => <Login/> } />
+        </Switch>
+        {/* <Login /> */}
+        {/* <SignUp createNewUser={this.props.createNewUser}/> */}
       </div>
     )
   }
 
 }
 
-export default UsersContainer
+const mapDispatchToProps = (dispatch) => ({
+  createNewUser: userInfo => dispatch(createNewUser(userInfo))
+})
+
+export default connect(null, mapDispatchToProps)(UsersContainer)
