a55b087129c28dc6c8143579bc1827982af8d0ad
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-03-31 11:17:26 -0400

    Add Router components.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import NoisemakerContainer from './containers/NoisemakerContainer'
 import UsersContainer from './containers/UsersContainer'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   // console.log(SUBMIT_URL)
   return (
-    <div className="App">
-      {/* <NoisemakerContainer /> */}
-      <UsersContainer />
-    </div>
+    <Router >
+      <Route exact path={['/login', '/signup']} component={UsersContainer} />
+      <Route exact path={['/noodlr', '/']} component={NoisemakerContainer} />
+    </Router>
   );
 }
 
