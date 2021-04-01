dcfb7655619093ebe12d2a844d594de18576d425
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-04-01 18:39:26 -0400

    Refactoring createNewUser action.

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -12,8 +12,16 @@ export const createNewUser = (user) => {
   }
 
   return (dispatch) => {
-    fetch(SUBMIT_URL + '/users', configObj).then(resp => resp.json()).then(json => {
-      dispatch(loginUser(json))
+    fetch(SUBMIT_URL + '/users', configObj).then(resp => {
+      if(resp.ok){
+        setToken(resp.headers.get("Authorization"))
+        return resp.json().then(json => dispatch({type: 'AUTHENTICATED', payload: json}))
+      }else{
+        return resp.json().then(errors => {
+          dispatch({type: 'NOT_AUTHENTICATED'})
+          return Promise.reject(errors)
+        })
+      }
     })
   }
 }
@@ -42,3 +50,17 @@ export const loginUser = (payload) => {
 export const logOutUser = () => {
   return {type: 'LOGOUT'}
 }
+
+const setToken = (token) => {
+  localStorage.setItem("token", token);
+  localStorage.setItem("lastLoginTime", new Date(Date.now()).getTime());
+};
+
+const getToken = () => {
+  const now = new Date(Date.now()).getTime();
+  const thirtyMinutes = 1000 * 60 * 30;
+  const timeSinceLastLogin = now - localStorage.getItem("lastLoginTime");
+  if (timeSinceLastLogin < thirtyMinutes) {
+    return localStorage.getItem("token");
+  }
+};
