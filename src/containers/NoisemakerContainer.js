2bbecfcd1c3b3799fa21cc6a7986c9fb63ae183d
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-04-02 11:35:19 -0400

    Make NoisemakerContainer a functional component.

--- src/containers/NoisemakerContainer.js
+++ src/containers/NoisemakerContainer.js
@@ -1,26 +1,13 @@
 import React, { Component } from 'react'
 import NoiseMaker from '../components/Noisemaker'
-import { connect } from 'react-redux'
-import { createVamp } from '../actions/vampActions'
 
 
-class NoisemakerContainer extends Component {
-
-  render(){
-    return(
-      <div id="noisemakerContainer">
-        <NoiseMaker createVamp={this.props.createVamp} />
-      </div>
-    )
-  }
-
+const NoisemakerContainer = () => {
+  return(
+    <div id="noisemakerContainer">
+      <NoiseMaker/>
+    </div>
+  )
 }
 
-// const mapStateToProps = (state) => {
-// }
-
-const mapDispatchToProps = (dispatch) => ({
-  createVamp: (configObj) => dispatch(createVamp(configObj))
-})
-
-export default connect(null, mapDispatchToProps)(NoisemakerContainer)
+export default NoisemakerContainer
