b8a0f9a6aeacf95b4c1d40053a766ab6e9af81de
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-04-01 23:09:53 -0400

    Import actions and dispatch to props.

--- src/containers/UsersContainer.js
+++ src/containers/UsersContainer.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Login from '../components/Login'
 import SignUp from '../components/SignUp'
-import { createNewUser } from '../actions/currentUserActions'
+import { createNewUser, loginUser } from '../actions/authActions'
 import {
-  BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Redirect
 } from "react-router-dom";
 
 class UsersContainer extends Component {
@@ -16,16 +15,26 @@ class UsersContainer extends Component {
     
   }
 
+  componentDidMount(){
+    this.props.getUser()
+    console.log('State: ')
+    console.log(this.props.getUser)
+  }
+
+  componentDidUpdate(){
+    this.props.getUser()
+    console.log('State: ')
+    console.log(this.props.state)
+  }
 
   render(){
     return(
       <div className="container" id="users">
         <Switch>
+          {this.props.loggedIn ? <Redirect to='/noodlr' /> : null}
           <Route path='/signup' render={() => <SignUp createNewUser={this.props.createNewUser}/>} />
-          <Route path='/login' render={() => <Login/> } />
+          <Route path='/login' render={() => <Login loginPostFetch={this.props.loginPostFetch} /> } />
         </Switch>
-        {/* <Login /> */}
-        {/* <SignUp createNewUser={this.props.createNewUser}/> */}
       </div>
     )
   }
@@ -33,7 +42,14 @@ class UsersContainer extends Component {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  createNewUser: userInfo => dispatch(createNewUser(userInfo))
+  createNewUser: userInfo => dispatch(createNewUser(userInfo)),
+  loginUser: userInfo => dispatch(loginUser(userInfo))
 })
 
-export default connect(null, mapDispatchToProps)(UsersContainer)
+const mapStateToProps = (state) => ({
+  state: state,
+  loggedIn: state.authReducer.loggedIn,
+  current_user: state.authReducer.current_user
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer)
