4b8df310f8f9c61e650e7327e05a3f5aca5fbf9c
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-04-02 11:30:54 -0400

    Build out Vamp actions and reducer.

--- src/actions/vampActions.js
+++ src/actions/vampActions.js
@@ -1,10 +1,59 @@
+import { getToken } from './authActions'
+
 const SUBMIT_URL = 'http://localhost:3001'
 
-export const createVamp = (configObj) => {
-  console.log('inside createVamp')
-  console.log(configObj)
+export const saveVamp = ({name, notation}) => {
+
+  const vamp = {
+    name: name,
+    notation: JSON.stringify(notation)
+  }
+  
+  const token = getToken()
+  // debugger
+  const configObj = {
+    method: "POST",
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({vamp})
+  }
+
   return (dispatch) => {
     dispatch({type: 'SENDING_VAMP'});
-    fetch(SUBMIT_URL + '/vamps', configObj).then(resp => resp.json()).then(json => console.log(json))
+    fetch(SUBMIT_URL + '/vamps', configObj).then(resp => {
+      if(resp.ok){
+        return resp.json().then(json => dispatch({type: "ADD_VAMP", payload: json}))
+      }else{
+        return resp.json().then(message => {
+          return dispatch({type:'FAILURE', payload: message})
+        })
+      }
+    })
+  }
+}
+
+export const getUserVamps = () => {
+  
+  const token = getToken()
+  const configObj = {
+    method: "GET",
+    headers: {
+      'Authorization': token,
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    }
+  }
+
+  return (dispatch) => {
+    fetch(SUBMIT_URL + '/vamps', configObj)
+      .then(
+        resp => resp.json()
+      )
+      .then(
+        json => dispatch({type: "ADD_VAMPS", payload: json})
+      )
   }
 }
--- src/reducers/vampReducer.js
+++ src/reducers/vampReducer.js
@@ -1,9 +1,21 @@
-export const vampReducer = (state = {vamps: [], status: null}, action) => {
+export const vampReducer = (
+  state = {
+    vamps: [],
+    pending: false,
+    error: null
+  }, action) => {
   switch(action.type){
-    case 'ADD_VAMP':
-      return
     case 'SENDING_VAMP':
-      return 
+      return {...state, pending: true}
+    case 'FAILURE':
+      const error = action.payload.message
+      return {...state, pending: false, error: error}
+    case 'CLEAR_ERRORS':
+      return {...state, pending: false,  error: null}
+    case 'ADD_VAMPS':
+      return {...state, vamps: action.payload}
+    case 'ADD_VAMP':
+      return {...state, vamps: [...state.vamps, action.payload.vamp]}
     default:
       return state
   }
