f2ca461f647bde7dbb5000750d5885526a83ef5c
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-04-06 18:10:56 -0400

    Make container conditionally include props depending on path.

--- src/containers/NoisemakerContainer.js
+++ src/containers/NoisemakerContainer.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react'
 import NoiseMaker from '../components/Noisemaker'
 
 
-const NoisemakerContainer = () => {
-  return(
-    <div id="noisemakerContainer">
-      <NoiseMaker/>
-    </div>
-  )
+const NoisemakerContainer = ({match}) => {
+  // console.log(match)
+
+  if(!!match && match.path === '/vamps/:id'){
+    const vampId = match.params.id
+    return(
+      <div id="noisemakerContainer">
+        <NoiseMaker vampId={vampId} />
+      </div>
+    )
+  }else{
+    return(
+      <div id="noisemakerContainer">
+        <NoiseMaker />
+      </div>
+    )
+  }
 }
 
 export default NoisemakerContainer
