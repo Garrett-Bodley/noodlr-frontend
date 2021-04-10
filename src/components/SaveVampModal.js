371a87ea26cbfb2e08c9ac0472218eb61d97a5f2
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-04-10 17:06:56 -0400

    Component now works for editing existing vamps.

--- src/components/SaveVampModal.js
+++ src/components/SaveVampModal.js
@@ -1,27 +1,29 @@
 import classNames from 'classnames/bind'
-import React, { Component } from 'react'
+// import React from 'react'
 
-const SaveVampModal = ({pending, error, hideModal, handleOnSubmit, handleOnChange}) => {
+const SaveVampModal = ({pending, status, error, vamp, hideModal, handleOnSubmit, handleOnChange}) => {
   return(
     <div id="save-vamp" className="modal is-active">
       <div className="modal-background" onClick={hideModal} ></div>
       <div className="modal-content">
         <header className="modal-card-head">
-          <p className="modal-card-title">Save Vamp</p>
+          <p className="modal-card-title">{!!vamp ? 'Edit Vamp' : 'Save Vamp'}</p>
           <button className="delete" onClick={hideModal} ></button>
         </header>
         <div className="modal-card-body">
           <form onSubmit={handleOnSubmit} onChange={handleOnChange}>
-            <p className="has-text-danger has-background-danger-light">{error ? `${error}` : null}</p>
+            <p className="has-text-danger has-background-danger-light">{!!error ? `${error}` : null}</p>
+            <p className="has-text-success has-background-success-light">{!!status ? `${status}` : null}</p>
+
             <div className="field">
               <label className="label">Name:</label>
               <div className="control">
-                <input className="input" name="name" type="text"></input>
+                <input className="input" defaultValue={!!vamp ? vamp.name : ''} name="name" type="text"></input>
               </div>
             </div>
             <div className="field">
               <div className="control">
-                <button className={classNames('button', 'is-link', {'is-loading': pending})} type="submit">Save</button>
+                <button className={classNames('button', 'is-link', {'is-loading': pending})} type="submit">{!!vamp ? 'Save Changes' : 'Save'}</button>
               </div>
             </div>
           </form>
