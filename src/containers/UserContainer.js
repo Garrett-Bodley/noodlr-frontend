98ed59f8f5e10b1bd36bb57c9ff0fa4e34b7d861
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-04-10 17:09:45 -0400

    Add lifecycle method to boot user if not logged in.

--- src/containers/UserContainer.js
+++ src/containers/UserContainer.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Route, Redirect } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Profile from '../components/Profile'
-import { getUserVamps } from '../actions/vampActions'
+import EditUser from '../components/EditUser'
+import { getUserVamps, deleteVamp, resetVampStatus } from '../actions/vampActions'
+import { getUser, patchUser, clearStatus, deleteUser } from '../actions/authActions'
 import './User.css'
 
 class UserContainer extends Component{
+
+  async componentDidMount(){
+    await this.props.getUser()
+    if(!this.props.loggedIn){
+      this.props.history.push('/login')
+    }
+  }
 
   componentDidUpdate(){
     if(!this.props.loggedIn){
@@ -13,27 +22,61 @@ class UserContainer extends Component{
     }
   }
 
+  componentWillUnmount(){
+    this.props.resetVampStatus()
+  }
+
   render(){
     return(
       <div id="user-container">
-        <Route 
+        <Switch>
+          <Route 
           exact path={`/users/${this.props.currentUser.id}`} 
-          render={() => <Profile history={this.props.history} currentUser={this.props.currentUser} vamps={this.props.vamps} getUserVamps={this.props.getUserVamps} />} 
-        />
-        <Route exact path={`/users/${this.props.currentUser.id}/edit`} />
+          render={() => 
+            <Profile 
+            history={this.props.history} 
+            currentUser={this.props.currentUser} 
+            vamps={this.props.vamps} 
+            getUserVamps={this.props.getUserVamps}
+            deleteVamp={this.props.deleteVamp}
+            vampStatus={this.props.vampStatus}
+            vampError={this.props.vampError}
+            />}
+          />
+          <Route exact path={`/users/${this.props.currentUser.id}/edit`}
+            render={() => 
+              <EditUser 
+              clearStatus={this.props.clearStatus}
+              status={this.props.status}
+              patchUser={this.props.patchUser} 
+              currentUser={this.props.currentUser}
+              deleteUser={this.props.deleteUser}
+              />
+            }
+          />
+        </Switch>
       </div>
     )
   }
 }
 
 const mapStateToProps = (state) => ({
+  vampStatus: state.vamp.status,
+  vampError: state.vamp.error,
   vamps: state.vamp.vamps,
   currentUser: state.auth.currentUser,
-  loggedIn: state.auth.loggedIn
+  loggedIn: state.auth.loggedIn,
+  status: state.auth.status
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  getUserVamps: () => dispatch(getUserVamps())
+  getUserVamps: () => dispatch(getUserVamps()),
+  deleteVamp: (vampId) => dispatch(deleteVamp(vampId)),
+  getUser: () => dispatch(getUser()),
+  patchUser: (user) => dispatch(patchUser(user)),
+  clearStatus: () => dispatch(clearStatus()),
+  deleteUser: (user) => dispatch(deleteUser(user)),
+  resetVampStatus: () => dispatch(resetVampStatus())
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserContainer)
