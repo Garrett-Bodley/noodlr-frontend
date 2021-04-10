277d6c6b7baf53644c0a7f76036d198f1971be51
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-04-10 17:08:23 -0400

    Container now redirects to user profile if already logged in.

--- src/containers/AuthContainer.js
+++ src/containers/AuthContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Login from '../components/Login'
 import SignUp from '../components/SignUp'
-import { createNewUser, loginUser } from '../actions/authActions'
+import { createNewUser, loginUser, clearStatus } from '../actions/authActions'
 import {
   Switch,
   Route,
@@ -11,13 +11,21 @@ import {
 
 class AuthContainer extends Component {
 
+  componentDidMount(){
+
+  }
+
+  componentWillUnmount(){
+
+  }
+
   render(){
     return(
       <div className="container" id="users">
         <Switch>
-          {this.props.loggedIn ? <Redirect to='/noodlr' /> : null}
-          <Route path='/signup' render={() => <SignUp createNewUser={this.props.createNewUser}/>} />
-          <Route path='/login' render={() => <Login loginUser={this.props.loginUser} /> } />
+          {this.props.loggedIn ? <Redirect to={`/users/${this.props.currentUser.id}`} /> : null}
+          <Route path='/signup' render={() => <SignUp clearStatus={this.props.clearStatus} status={this.props.status} createNewUser={this.props.createNewUser}/>} />
+          <Route path='/login' render={() => <Login clearStatus={this.props.clearStatus} status={this.props.status} loginUser={this.props.loginUser} /> } />
         </Switch>
       </div>
     )
@@ -26,13 +34,15 @@ class AuthContainer extends Component {
 
 const mapDispatchToProps = (dispatch) => ({
   createNewUser: userInfo => dispatch(createNewUser(userInfo)),
-  loginUser: userInfo => dispatch(loginUser(userInfo))
+  loginUser: userInfo => dispatch(loginUser(userInfo)),
+  clearStatus: () => dispatch(clearStatus())
 })
 
 const mapStateToProps = (state) => ({
   state: state,
   loggedIn: state.auth.loggedIn,
-  current_user: state.auth.current_user
+  currentUser: state.auth.currentUser,
+  status: state.auth.status
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(AuthContainer)
