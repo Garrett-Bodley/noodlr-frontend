20290076a046be777060036ac33b5e577c278771
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-04-10 17:07:37 -0400

    Add in password confirmation field. Clear store status w/lifecycle methods.

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,8 +3,17 @@ import React, { Component } from 'react'
 class Signup extends Component{
   
   state = {
-    username: null,
-    password: null
+    username: '',
+    password: '',
+    passwordConfirmation: ''
+  }
+
+  componentDidMount(){
+    this.props.clearStatus()
+  }
+
+  componentWillUnmount(){
+    this.props.clearStatus()
   }
 
   handleOnChange = (e) => {
@@ -13,26 +22,39 @@ class Signup extends Component{
 
   handleOnSubmit = (e) => {
     e.preventDefault();
-    this.props.createNewUser(this.state)
+    this.props.createNewUser({
+      username: this.state.username,
+      password: this.state.password,
+      password_confirmation: this.state.passwordConfirmation
+    })
   }
 
   render(){
     return(
-      <div id="login" className="container card box">
-        <form onSubmit={this.handleOnSubmit} onChange={this.handleOnChange} >
+      <div id="SignUp" className="container card box login">
+        <form onSubmit={this.handleOnSubmit} >
           <div className="field">
             <h2 className="is-size-2">Sign Up</h2>
           </div>
           <div className="field">
+            <p className="has-text-danger has-background-danger-light">{!!this.props.status.failed ? `${this.props.status.messages}` : null}</p>
+          </div>
+          <div className="field">
             <label>Username:</label>
             <div className="control">
-              <input name="username" type="text"></input>
+              <input name="username" className="input" onChange={this.handleOnChange} value={this.state.username} type="text"></input>
             </div>
           </div>
           <div className="field">
             <label>Password:</label>
             <div className="control">
-              <input name="password" type="password"></input>
+              <input name="password" className="input" onChange={this.handleOnChange} value={this.state.password} type="password"></input>
+            </div>
+          </div>
+          <div className="field">
+            <label>Password Confirmation:</label>
+            <div className="control">
+              <input name="passwordConfirmation" className="input" onChange={this.handleOnChange} value={this.state.passwordConfirmation} type="password"></input>
             </div>
           </div>
           <div className="field">
