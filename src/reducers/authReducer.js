995511fcf71de7e5bb335c00dcb8405427a4283b
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-04-10 17:10:06 -0400

    Add edit functionality as well as status cases.

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,4 +1,10 @@
-export const authReducer = (state = {loggedIn: false, authChecked: false, currentUser: {}}, action) => {
+export const authReducer = (
+  state = {
+    loggedIn: false, 
+    authChecked: false, 
+    currentUser: {},
+    status: {}
+  }, action) => {
   switch(action.type){
     case "AUTHENTICATED":
       return ({...state,
@@ -9,7 +15,9 @@ export const authReducer = (state = {loggedIn: false, authChecked: false, curren
     case "NOT_AUTHENTICATED":
       return ({...state,
         authChecked: true,
-        loggedIn: false
+        loggedIn: false,
+        currentUser: {},
+        status: {failed: true, messages: [...action.payload]}
       })
     case "AUTHENTICATION_PERSISTED":
       return ({...state,
@@ -27,6 +35,28 @@ export const authReducer = (state = {loggedIn: false, authChecked: false, curren
         loggedIn: false,
         currentUser: {}
       }
+    case "EDIT_USER":
+      return {
+        ...state,
+        loggedIn: true,
+        authChecked: true, 
+        currentUser: action.payload, 
+        status: {
+          success: true, 
+          messages: ["Update Successful!"]
+        }
+      }
+    case "UPDATE_FAILED":
+      return {...state, status: {failed: true, messages: [...action.payload]}}
+    case "USER_DELETED":
+      return {
+        ...state, 
+        loggedIn: false, 
+        currentUser: {}, 
+        status: {success: true, messages: [...action.payload]}
+      }
+    case "CLEAR_STATUS":
+      return {...state, status: {}}
     default:
       return state
   }
