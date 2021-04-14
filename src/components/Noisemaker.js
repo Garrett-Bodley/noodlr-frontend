abf48c4dddcc358e155bae63a89647b61fd063a5
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-04-14 19:52:18 -0400

    Remove commented out code.

--- src/components/Noisemaker.js
+++ src/components/Noisemaker.js
@@ -143,43 +143,6 @@ class NoiseMaker extends Component {
     this.setState({rows: newRows})
   }
 
-  // renderGrid = () => {
-  //   let colors = [
-  //     ['239, 50, 217', '137, 255, 253'],
-  //     ['239, 50, 217', '137, 255, 253'],
-
-  //     ['5, 118, 230', '0, 242, 97'],
-  //     ['5, 118, 230', '0, 242, 97'],
-      
-  //     ['194, 21, 0', '255, 197, 0'],
-  //     ['194, 21, 0', '255, 197, 0'],
-
-  //     ['40, 60, 134', '255, 0, 204'],
-  //     ['40, 60, 134', '255, 0, 204'],
-  //     ['40, 60, 134', '255, 0, 204'],
-  //   ]
-
-    
-  //   let grid = this.state.grid.map((row, rowIndex) => {
-  //     return <div key={rowIndex} className="note-row">
-  //       {row.map(({note, isActive}, noteIndex) => {
-  //         return(
-  //           <Note 
-  //           color={colors[rowIndex]} 
-  //           className={classNames("note", {"on-beat": this.state.beat === (noteIndex + 1) % 16}, {'note-is-active': isActive})} 
-  //           key={noteIndex + note} 
-  //           isActive={isActive} 
-  //           note={note} 
-  //           onClick={() => this.handleOnClick(rowIndex, noteIndex)} 
-  //           />
-  //         )
-  //       })}
-  //     </div>
-  //   })
-
-  //   return grid
-  // }
-
   togglePlay = () => {
     if(!this.state.activated){
       Tone.start()
