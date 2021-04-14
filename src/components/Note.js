4547e2d8fa82f16cfbbf850cbb9b11f9b689a35c
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-04-14 19:54:48 -0400

    Convert Note into a functional component.

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,21 +1,16 @@
-import { Component } from 'react'
-
-class Note extends Component{
-  render(){
-    const styling = 
-      this.props.isActive ? 
-        {backgroundImage: `linear-gradient(rgba(${this.props.color[1]}, 1), rgba(${this.props.color[0]}, 1)`} : 
-        {backgroundImage: `linear-gradient(rgba(${this.props.color[1]}, 1), rgba(${this.props.color[1]}, 1)`}
-    return(
-      <button 
-      style={styling} 
-      onClick={this.props.onClick}
-      className={this.props.className}>
-      {/* {this.props.note} */}
-      </button>
-    )
-  }
-
+const Note = ({isActive, color, onClick, className}) => {
+  const styling = 
+    isActive ? 
+      {backgroundImage: `linear-gradient(rgba(${color[1]}, 1), rgba(${color[0]}, 1)`} : 
+      {backgroundImage: `linear-gradient(rgba(${color[1]}, 1), rgba(${color[1]}, 1)`};
+  
+  return(
+    <button 
+    style={styling} 
+    onClick={onClick}
+    className={className}>
+    </button>
+  )
 }
 
 export default Note
