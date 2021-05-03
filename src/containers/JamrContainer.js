06b558f8dd529b8a03504099131cfc43bc1744c5
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-05-03 14:31:32 -0400

    Stub out JamrContainer.

---
+++ src/containers/JamrContainer.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class JamrContainer extends Component{
+
+  makeKeys = () => {
+    const keys = []
+    for(let i = 0; i < 8; i++){
+      let key = <div id={`key-${i}`}></div>
+    }
+
+  }
+
+
+  render(){
+    return(
+      <div id="jammr-container">
+
+
+
+
+
+      </div>
+    )
+  }
+
+}
+
+export default connect()(JamrContainer)
