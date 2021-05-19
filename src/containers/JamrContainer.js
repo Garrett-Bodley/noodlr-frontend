d26d489bd15dc125e84733709e4d97126974245f
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-05-19 18:18:12 -0400

    Create mousdown, mouseup, and mousout functions. Create keyboard controls and fix synth.

--- src/containers/JamrContainer.js
+++ src/containers/JamrContainer.js
@@ -6,40 +6,65 @@ import Key from '../components/Jamr/Key'
 class JamrContainer extends Component{
 
   state = {
-    synths: JamrContainer.makeSynths(),
-    keyboardControls: 
+    synth: JamrContainer.makeSynth(),
+    // keyboardControls: JamrContainer.makeKeyboardControls()
   }
 
-  static makeSynths = () => {
-    const synths = []
-    for(let i = 0; i < 12; i++){
-      let synth = new Tone.Synth({oscillator: {type: 'triangle8'}}).toDestination()
-      synths.push(synth)
-    }
-    return synths
+  static makeSynth = () => {
+    return new Tone.PolySynth(Tone.Synth).set({ oscillator: { type: 'triangle8' } }).toDestination()
   }
 
-  static makeKeyboardControls = () => {
-    let notes = ['F3', 'Gb3', 'G3', 'Ab3', 'A3', 'Bb3', 'B3', 'C4', 'Db4', 'D4', 'Eb4', 'E4', 'F4']
-    let keys = ['a', 'w', 's', 'e', 'd', 'f', 'g', 'y', 'h', 'u', 'j', 'i', 'k']
-    const keyboardControls = {}
-    for(let i = 0; i < notes.length; i++){
-      keyboardControls[keys[i]] = {pressed: false, note: notes[i]}
-    }
-    return keyboardControls
+  // static makeKeyboardControls = () => {
+  //   let notes = ['F3', 'Gb3', 'G3', 'Ab3', 'A3', 'Bb3', 'B3', 'C4', 'Db4', 'D4', 'Eb4', 'E4', 'F4']
+  //   let keys = ['a', 'w', 's', 'e', 'd', 'f', 'g', 'y', 'h', 'u', 'j', 'i', 'k']
+  //   const keyboardControls = {}
+  //   for(let i = 0; i < notes.length; i++){
+  //     keyboardControls[keys[i]] = {pressed: false, note: notes[i]}
+  //   }
+  //   return keyboardControls
+  // }
 
+  // handleKeyDown = (e) => {
+  //   const key = e.key.toLowerCase();
+  //   if(this.state.keyboardControls[key] && !this.state.keyboardControls[key].pressed){
+  //     this.state.synth.triggerAttack(this.state.keyboardControls[key].note)
+  //     const newControls = Object.assign({}, this.state.keyboardControls)
+  //     newControls[key].pressed = true
+  //     this.setState( { keyboardControls: newControls } )
+  //   }
+  // }
+
+  // handleKeyUp = (e) => {
+  //   const key = e.key.toLowerCase();
+  //   if(this.state.keyboardControls[key] && this.state.keyboardControls[key].pressed){
+  //     this.state.synth.triggerRelease(this.state.keyboardControls[key].note)
+  //     const newControls = Object.assign({}, this.state.keyboardControls)
+  //     this.setState( { keyboardControls: newControls } )
+  //   }
+  // }
+  
+  handleMouseDown = (note) => {
+    this.state.synth.triggerAttack(note)
   }
 
-  handleOnClick = (note) => {
-
+  handleMouseUp = (note) => {
+    this.state.synth.triggerRelease(note)
   }
 
-  makeKeys = () => {
-    const keys = []
-    for(let i = 0; i < 8; i++){
-      keys.push(<Key onClick={(note) => this.handleOnClick(note)} note={} />)
-    }
+  handleMouseOut = (note) => {
+    this.state.synths.triggerRelease(note)
+  }
 
+  makeWhiteKeys = () => {
+    let notes = ['F3', 'G3', 'A3', 'B3', 'C4', 'D4', 'E4', 'F4']
+    return notes.map(note => 
+      <Key 
+        note={note} 
+        handleMouseDown={() => this.handleMouseDown(note)} 
+        handleMouseUp={() => this.handleMouseUp(note)}
+        handleMouseOut={() => this.handleMouseOut(note)} 
+      />
+    )
   }
 
 
@@ -47,10 +72,10 @@ class JamrContainer extends Component{
     return(
       <div id="jammr-container">
 
-
-
-
-
+        <div className="white-keys">
+          {this.makeWhiteKeys()}
+        </div>
+        
       </div>
     )
   }
