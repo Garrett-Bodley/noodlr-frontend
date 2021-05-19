83a146e9b6a03c7dd13be96d0874752f9e40dbc0
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-05-19 18:18:44 -0400

    Make key component a fancy button with stacked spans and checkbox.

--- src/components/Jamr/Key.js
+++ src/components/Jamr/Key.js
@@ -1,7 +1,20 @@
-const Key = ({note}) => {
+import './Key.css'
+
+const Key = ({note, handleMouseDown, handleMouseUp, handleMoustOut}) => {
   return(
     <div>
+      <label 
+        className="pushable"
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseOut={handleMoustOut}
+      >
+        <input type="checkbox" className="checkbox" hidden={true}></input>
+        <span className="shadow"></span>
+        <span className="edge"></span>
+        <span className="front">{note}</span>
 
+      </label>
     </div>
   )
 }
