30ba4759f7bc0821ee21d85e6cd074efbce095f5
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-06-06 17:17:08 -0400

    Pass query to Noisemaker if present.

--- src/containers/JamrContainer.js
+++ src/containers/JamrContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 import NoiseMaker from '../components/Noisemaker/Noisemaker'
 
 
@@ -9,6 +10,12 @@ class JamrContainer extends Component{
     if(!!this.props.match && this.props.match.path === '/vamps/:id'){
       const vampId = parseInt(this.props.match.params.id)
       return <NoiseMaker vampId={vampId} />
+    }else if(!!this.props.location.search){
+      if(this.props.location.search.substr(0, 6) === '?vamp='){
+        return <NoiseMaker query={this.props.location.search.substr(6)} />
+      }else{
+        return <Redirect to='/'/>
+      }
     }else{
       return <NoiseMaker />
     }
