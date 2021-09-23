0dc45e01a8949e209c4bf0150132f29aecab55d7
Garrett-Bodley <garrett.bodley@gmail.com>
2021-09-23 14:45:42 -0400

    Import new CSS file. use prettier formatter.

--- src/containers/JamrContainer.js
+++ src/containers/JamrContainer.js
@@ -1,37 +1,36 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { Redirect } from 'react-router-dom'
-import NoiseMaker from '../Noisemaker/Noisemaker'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+import NoiseMaker from "../Noisemaker/Noisemaker";
+import "./JamrContainer.css";
 
-
-class JamrContainer extends Component{
-
+class JamrContainer extends Component {
   renderNoiseMaker = () => {
-    if(!!this.props.match && this.props.match.path === '/vamps/:id'){
-      const vampId = parseInt(this.props.match.params.id)
-      return <NoiseMaker vampId={vampId} />
-    }else if(!!this.props.location.search){
-      if(this.props.location.search.substr(0, 6) === '?vamp='){
-        return <NoiseMaker queryString={this.props.location.search.substr(6)} />
-      }else{
-        return <Redirect to='/'/>
+    if (!!this.props.match && this.props.match.path === "/vamps/:id") {
+      const vampId = parseInt(this.props.match.params.id);
+      return <NoiseMaker vampId={vampId} />;
+    } else if (!!this.props.location.search) {
+      if (this.props.location.search.substr(0, 6) === "?vamp=") {
+        return (
+          <NoiseMaker queryString={this.props.location.search.substr(6)} />
+        );
+      } else {
+        return <Redirect to="/" />;
       }
-    }else{
-      return <NoiseMaker />
+    } else {
+      return <NoiseMaker />;
     }
-  }
+  };
 
-  render(){
-    return(
-      <div id="noisemakerContainer">
-        {this.renderNoiseMaker()}
-      </div>
-    )
+  render() {
+    return (
+      <div className="noisemaker-container">{this.renderNoiseMaker()}</div>
+    );
   }
 }
 
 const mapStatetoProps = (state) => ({
-  loggedIn: state.loggedIn
-})
+  loggedIn: state.loggedIn,
+});
 
-export default connect(mapStatetoProps)(JamrContainer)
+export default connect(mapStatetoProps)(JamrContainer);
