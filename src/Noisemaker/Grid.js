8ad5e9091531a413dd47884ef24e4d26c21810d3
Garrett-Bodley <garrett.bodley@gmail.com>
2021-09-30 15:18:05 -0400

    Stub out new Grid component for refactoring.

---
+++ src/Noisemaker/Grid.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Article = styled.article`
+  
+`
+
+const Grid = ({rowCount}) => {
+
+
+
+  return (
+    <article>
+      
+    </article>
+  )
+}
+
+export default Grid
