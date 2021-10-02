67019ad4f168f75dc34e52b6b80278f24b62364d
Garrett-Bodley <garrett.bodley@gmail.com>
2021-10-02 19:10:18 -0400

    Stub out a SynthContext and move synth utilities to their own folder.

--- src/utilities/SynthUtilities/SynthContext.js
+++ src/utilities/SynthUtilities/SynthContext.js
@@ -1,14 +1,20 @@
-import React, { useContext, useRef } from 'react';
+import React, { useContext, useRef, useState } from 'react';
+import * as Tone from "tone";
 import makeSynths from './makeSynths';
 
 const SynthContext = React.createContext()
+
+const toneLoop = (time) => {
+  const [beat, setBeat] = useState(0)
+
+}
 
 export const SynthProvider = ({children}) => {
 
   const synths = useRef(makeSynths())
 
   return (
-    <SynthContext.Provider value={synths}>
+    <SynthContext.Provider value={synths.current}>
       {children}
     </SynthContext.Provider>
   )
---
+++ src/utilities/SynthUtilities/useToneLoop.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+const useToneLoop = (time) => {
+
+  const [beat, setBeat] = useState(0)
+
+}
+
+export default useToneLoop
