c086c0e24c070ab5240233138b02c4c49b67b51d
Garrett-Bodley <garrett.bodley@gmail.com>
2021-10-03 18:42:30 -0400

    Change names of variables.

--- src/utilities/makeGrid.js
+++ src/utilities/makeGrid.js
@@ -1,16 +1,16 @@
 const makeGrid = () => {
-  const notes = ["F4", "Eb4", "C4", "Bb3", "Ab3", "F3", "C3", "C2", "C1"];
-  const rows = [];
+  const pitches = ["F4", "Eb4", "C4", "Bb3", "Ab3", "F3", "C3", "C2", "C1"];
+  const vamp = [];
 
-  for (const note of notes) {
+  for (const pitch of pitches) {
     const row = [];
     for (let i = 0; i < 16; i++) {
-      row.push({ note: note, isActive: false });
+      row.push({ pitch: pitch, isActive: false });
     }
-    rows.push(row);
+    vamp.push(row);
   }
 
-  return rows;
+  return vamp;
 };
 
 export default makeGrid;
