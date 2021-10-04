6318122d20717658aae2b5f2fd98432f3e400195
Garrett-Bodley <garrett.bodley@gmail.com>
2021-10-04 02:37:46 -0400

    Make shadows sexy.

--- src/Noisemaker/Tone/Tone.js
+++ src/Noisemaker/Tone/Tone.js
@@ -19,7 +19,11 @@ const Button = styled.button`
     border: none;
     border-radius: 4px;
     background-color: ${(props) => props.activeColor || "#38CC77"};
-    box-shadow: inset -1px 1px 3px 0px #5e5e5e;
+    box-shadow: 
+      inset 0.6px 0.1px 0.3px rgba(20, 31, 25, 0.36),
+      inset 0.13px 0.4px 0.9px rgba(20, 31, 25, 0.54),
+      inset 0.5px 1px 4px rgba(20, 31, 25, 0.09);
+
     opacity: ${(props) => (props.isPressed ? "100%" : "0%")};
     will-change: opacity;
     transition: opacity 100ms ease;
@@ -33,7 +37,10 @@ const Button = styled.button`
     border: none;
     border-radius: 4px;
     background-color: lightgray;
-    box-shadow: -1px 2px 3px 0px #5e5e5e;
+    box-shadow: 
+      0.1px 0.1px 0.3px rgba(0, 0, 0, 0.238),
+      0.2px 0.4px 0.9px rgba(0, 0, 0, 0.352), 
+      1px 2px 4px rgba(0, 0, 0, 0.59);
     opacity: ${(props) => (props.isPressed ? "0%" : "100%")};
     ${"" /* opacity: 0%; */}
     will-change: opacity;
@@ -44,10 +51,14 @@ const Button = styled.button`
 const Tone = (props) => {
   const [isPressed, setIsPressed] = useState(false);
   const toggleNote = useVampUpdate();
+  const { rowNum, beatNum, timing, color } = props;
+  const transitionStyle = {
+    transition: `transform${timing}`,
+  };
 
   const handleOnClick = () => {
     setIsPressed((prevState) => !prevState);
-    toggleNote(props.rowNum, props.beatNum);
+    toggleNote(rowNum, beatNum);
   };
 
   return (
@@ -60,8 +71,9 @@ const Tone = (props) => {
         height="100%"
         border="1px solid black"
         bg="lightgray"
-        activeColor={props.color}
+        activeColor={color}
         borderRadius="4px"
+        p="0"
       />
     </AspectRatioContainer>
   );
