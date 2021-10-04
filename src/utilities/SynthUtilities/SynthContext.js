bbf155fa8034f6c54c4e3d3cb9f9cab1401e9151
Garrett-Bodley <garrett.bodley@gmail.com>
2021-10-03 23:34:11 -0400

    Build out SynthContext. Utilize useVamp hook.

--- src/utilities/SynthUtilities/SynthContext.js
+++ src/utilities/SynthUtilities/SynthContext.js
@@ -1,17 +1,36 @@
-import React, { useContext, useRef, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import * as Tone from "tone";
 import makeSynths from './makeSynths';
+import { useVamp } from '../VampUtilities/VampProvider'
+import makeGrid from '../makeGrid';
 
 const SynthContext = React.createContext()
 
-const toneLoop = (time) => {
-  const [beat, setBeat] = useState(0)
-
+const toneConfig = (repeat, tempo,) => {
+  Tone.Transport.bpm = tempo
+  Tone.Transport.scheduleRepeat(repeat, "8n")
 }
 
 export const SynthProvider = ({children}) => {
 
+  const [beat, setBeat] = useState(0)
   const synths = useRef(makeSynths())
+  const [tempo, setTempo] = useState(120)
+  const vamp = useVamp()
+
+  useEffect(() => {
+    toneConfig(repeat, tempo)
+  })
+
+  const repeat = (time) => {
+    vamp.forEach((row, index) => {
+      let tone = row[beat]
+      if(tone.isActive){
+        synths.current[index].triggerAttackRelease(tone.pitch, "8n", time)
+      }
+    })
+    setBeat(beat + 1)
+  }
 
   return (
     <SynthContext.Provider value={synths.current}>
