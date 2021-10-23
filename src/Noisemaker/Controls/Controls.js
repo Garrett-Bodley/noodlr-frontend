d1ed354ad947761db5604a532a2d1fa6e2a53f3f
Garrett-Bodley <garrett.bodley@gmail.com>
2021-10-23 13:48:44 -0400

    Manage tempo and volume via context provider hooks.

--- src/Noisemaker/Controls/Controls.js
+++ src/Noisemaker/Controls/Controls.js
@@ -1,9 +1,15 @@
 import React from "react";
 import styled from "styled-components";
-import { color, flexbox, grid, space, layout } from "styled-system";
+import { border, color, flexbox, grid, space, typography, layout } from "styled-system";
 import { VideoPlayPause } from "@styled-icons/fluentui-system-filled/VideoPlayPause";
-import { useToneUpdate } from "../../utilities/ToneContext";
+import { useTempo, useTempoUpdate, useVolume, useVolumeUpdate } from "../../utilities/ToneProvider";
+import PlayPauseButton from "./PlayPauseButton";
+import VolumeIcon from "./VolumeIcon"
+import MetronomeIcon from "./MetronomeIcon"
+import Range from "./Range"
 const Article = styled.article`
+  ${border}
+  ${color};
   ${space};
   ${layout};
   ${flexbox}
@@ -16,18 +22,51 @@ const StyledPlayPause = styled(VideoPlayPause)`
   ${space};
 `;
 
+const Container = styled.div`
+  ${border};
+  ${flexbox};
+  ${layout};
+  ${space};
+  ${typography};
+`
+
 const Controls = (props) => {
-  const togglePlay = useToneUpdate();
+
+  const tempo = useTempo()
+  const updateTempo = useTempoUpdate()
+  const volume = useVolume()
+  const updateVolume = useVolumeUpdate()
+
+  const handleTempoChange = (e) => {
+    updateTempo(parseFloat(e.target.value))
+  }
+
+  const handleVolumeChange = (e) => {
+    updateVolume(e.target.value === "0" ? -Infinity : e.target.value - 45)
+  }
 
   return (
     <Article
+      bg="#f8f8ff"
       {...props}
       display="flex"
+      flexDirection="column"
       justifyContent="center"
       alignItems="center"
       gridTemplateColumns=""
+      borderRadius="3px"
+      my="1em"
+      border="1px solid black"
     >
-      <StyledPlayPause width="2em" onClick={togglePlay} />
+      <Container display="flex" alignItems="center" justifyContent="center" width="100%">
+        <MetronomeIcon className="icon" fill="grey" strokeWidth="5px" />
+        <Range eventHandler={handleTempoChange} min={60} max={180} value={tempo} />
+      </Container>
+      <Container display="flex" textAlign="left" alignItems="center" justifyContent="center" width="100%">
+        <VolumeIcon className="icon"/>
+        <Range eventHandler={handleVolumeChange} min={0} max={50} value={volume === -Infinity ? 0 : volume + 45} />
+      </Container>
+      <PlayPauseButton/>
     </Article>
   );
 };
